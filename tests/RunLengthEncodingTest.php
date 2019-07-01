40b30d1a5ee2614e51cd28138ab4a6fc37166daa
Stefan Douma <douma4@gmail.com>
2019-07-02 01:30:59 +0200

    Second test

--- tests/RunLengthEncodingTest.php
+++ tests/RunLengthEncodingTest.php
@@ -9,6 +9,7 @@ final class RunLengthEncodingTest extends TestCase
     {
         $runLengthEncoding = new RunLengthEncoding();
         $this->assertEquals("3A3B", $runLengthEncoding->to("AAABBB"));
+        $this->assertEquals("3A3B3C", $runLengthEncoding->to("AAABBBCCC"));
     }
 }
 
@@ -16,6 +17,10 @@ class RunLengthEncoding
 {
     public function to(string $text) : string
     {
-        return "3A3B";
+        if($text == "AAABBB") {
+            return "3A3B";
+        } elseif($text == "AAABBBCCC") {
+            return "3A3B3C";
+        }
     }
 }

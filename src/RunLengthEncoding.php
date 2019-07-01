6014ec6880c27294711912d7810a48dbae1e695f
Stefan Douma <douma4@gmail.com>
2019-07-02 01:41:56 +0200

    Moved class

---
+++ src/RunLengthEncoding.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace RunLengthEncoding;
+
+class RunLengthEncoding
+{
+    public function to(string $text) : string
+    {
+        $output = "";
+        $prevChar = false;
+        $count = 0;
+        foreach(str_split($text,1) as $char) {
+            if($char !== $prevChar) {
+                if($prevChar) {
+                    $output .= "{$count}{$prevChar}";
+                }
+                $count = 1;
+                $prevChar = $char;
+            } else {
+                $count++;
+            }
+            $prevChar = $char;
+        }
+        $output .= "{$count}{$prevChar}";
+        return $output;
+    }
+}
--- tests/RunLengthEncodingTest.php
+++ tests/RunLengthEncodingTest.php
@@ -7,35 +7,11 @@ final class RunLengthEncodingTest extends TestCase
 {
     public function test(): void
     {
-        $runLengthEncoding = new RunLengthEncoding();
+        $runLengthEncoding = new \RunLengthEncoding\RunLengthEncoding();
         $this->assertEquals("3A3B", $runLengthEncoding->to("AAABBB"));
         $this->assertEquals("3A3B3C", $runLengthEncoding->to("AAABBBCCC"));
         $this->assertEquals("3A3B3C1D", $runLengthEncoding->to("AAABBBCCCD"));
         $this->assertEquals('12W1B12W3B24W1B14W',
             $runLengthEncoding->to("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW"));
-    }
-}
-
-class RunLengthEncoding
-{
-    public function to(string $text) : string
-    {
-        $output = "";
-        $prevChar = false;
-        $count = 0;
-        foreach(str_split($text,1) as $char) {
-            if($char !== $prevChar) {
-                if($prevChar) {
-                    $output .= "{$count}{$prevChar}";
-                }
-                $count = 1;
-                $prevChar = $char;
-            } else {
-                $count++;
-            }
-            $prevChar = $char;
-        }
-        $output .= "{$count}{$prevChar}";
-        return $output;
     }
 }
